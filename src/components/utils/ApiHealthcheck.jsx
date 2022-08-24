9604de916d27654356dd7849cb72b3e2c54e59ae
Tolga Köyluoglu <tolga.koyluoglu1@gmail.com>
2022-08-24 09:12:11 +0200

    fix: add axios helper func

--- src/components/utils/ApiHealthcheck.jsx
+++ src/components/utils/ApiHealthcheck.jsx
@@ -1,14 +1,14 @@
 import { useEffect, useState } from 'react'
+import api from '../../api'
 
 export default function ApiHealthcheck() {
   const [healthData, setHealthData] = useState('Loading...')
 
   useEffect(() => {
-    fetch('/api/')
-      .then((res) => res.json())
+    api.get('/')
       .then((result) => {
-        console.log('result = ', result)
-        const jsonText = JSON.stringify(result, null, 2)
+        console.log('result = ', result.data)
+        const jsonText = JSON.stringify(result.data, null, 2)
         setHealthData(jsonText)
       })
       .catch((error) => console.log(error))
