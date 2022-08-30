bc49a4c37e754abf5d64200b04276ff8f594a23e
Thomas Sandberg <thomas.sandberg@regent.se>
2022-08-30 11:43:10 +0200

    Add NotFound route

---
+++ src/routes/NotFound.jsx
@@ -0,0 +1,26 @@
+export default function NotFound() {
+  // const codeParam = new URLSearchParams(document.location.search).get('code')
+
+  // const code = codeParam ? codeParam : ''
+
+  // const errorMessage =
+  //   code +
+  //   ' ' +
+  //   (code === '400'
+  //     ? 'Bad Request'
+  //     : code === '401'
+  //     ? 'Unauthorized'
+  //     : code === '403'
+  //     ? 'Forbidden'
+  //     : code === '404'
+  //     ? 'Not Found'
+  //     : 'Unknown')
+
+  return (
+    <main>
+      <h2>Not found!</h2>
+      <small>Have a coffee and try again...</small>
+      {/* <p style={{ paddingTop: '0.5em', fontSize: '3em' }}>{`${errorMessage}`}</p> */}
+    </main>
+  )
+}
