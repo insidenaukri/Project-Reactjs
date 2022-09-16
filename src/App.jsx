070aa7e0ad1a7b27f7c92501d82d3d90f8b7a54c
Thomas Sandberg <thomas.sandberg@regent.se>
2022-09-16 15:16:51 +0200

    feat: Add template for user context

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { Footer } from './components/footer/'
 import { Sidebar } from './components/sidebar/'
 import { Snackbar } from './components/snackbar/'
 import { Organisations } from './routes/organisations/'
+import { UserProvider } from './contexts/user-context'
 
 export function App() {
   return (
@@ -17,16 +18,25 @@ export function App() {
       <Snackbar />
       <div className="page-wrapper">
         <Sidebar />
+        {/* <UserProvider> */}
         <Routes>
           <Route path="/" element={<Bonuses />} />
           <Route path="/bonuses/:bonusId" element={<BonusDetails />} />
           <Route path="/time-entries" element={<TimeEntries />} />
           <Route path="/organisations" element={<Organisations />} />
-          <Route path="/dummy" element={<Dummy />} />
+          <Route
+            path="/dummy"
+            element={
+              <UserProvider>
+                <Dummy />
+              </UserProvider>
+            }
+          />
           <Route path="/healthcheck" element={<Healthcheck />} />
           <Route path="/error" element={<HttpError />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
+        {/* </UserProvider> */}
       </div>
       <Footer />
     </BrowserRouter>
---
+++ src/contexts/user-context.jsx
@@ -0,0 +1,58 @@
+import { useReducer } from 'react'
+import { createContext, useContext, useContextSelector } from 'use-context-selector'
+
+const ACTION_PREFIX = 'user/'
+
+export const UserActions = {
+  USER_SET: `${ACTION_PREFIX}USER_SET`,
+}
+
+const initialState = {
+  id: '',
+  email: '',
+  organisationId: '',
+}
+
+const UserContext = createContext()
+
+function userReducer(state, { type, payload: { id, email, organisationId } }) {
+  switch (type) {
+    case UserActions.USER_SET: {
+      return {
+        id,
+        email,
+        organisationId,
+      }
+    }
+    default: {
+      throw new Error(`Action type unknown: ${type}`)
+    }
+  }
+}
+
+export function UserProvider({ children }) {
+  const [state, dispatch] = useReducer(userReducer, initialState)
+  const value = [state, dispatch]
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>
+}
+
+export function useUser() {
+  const context = useContext(UserContext)
+
+  if (!context) {
+    throw new Error(`${useUser.name} requires ${UserContext.name}!`)
+  }
+  return context
+}
+
+// COMMANDS
+export const updateUser = (dispatch, id, email, organisationId) => {
+  dispatch({ type: UserActions.USER_SET, payload: { id, email, organisationId } })
+}
+
+// SELECTORS
+export const selectUser = () => useContextSelector(UserContext, (c) => c[0])
+export const selectUserId = () => useContextSelector(UserContext, (c) => c[0].id)
+export const selectUserEmail = () => useContextSelector(UserContext, (c) => c[0].email)
+export const selectOrganisationId = () => useContextSelector(UserContext, (c) => c[0].organisationId)
