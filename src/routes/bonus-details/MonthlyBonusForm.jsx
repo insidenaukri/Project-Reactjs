c67d9e974de4a815eb4ff73dfaf27b7eaf0fd5fa
Tolga Köyluoglu <tolga.koyluoglu1@gmail.com>
2022-09-16 15:51:18 +0200

    feat: add update, delete and recalculate bonus

--- src/routes/bonus-details/MonthlyBonusForm.jsx
+++ src/routes/bonus-details/MonthlyBonusForm.jsx
@@ -2,24 +2,25 @@ import React, { useEffect, useState } from 'react'
 import { Input } from '../../components/input/'
 import styles from './MonthlyBonusForm.module.css'
 
-export function MonthlyBonusForm({ monthlyBonus, employee }) {
+export function MonthlyBonusForm({ monthlyBonus, employee, updatedBonus }) {
   const [bonusData, setBonusData] = useState({
-    reported_time: 0,
-    travel_time: 0,
-    unassigned_time: 0,
-    vacation_time: 0,
-    sick_time: 0,
-    offduty_time: 0,
-    comp_time: 0,
-    value_of_vacation: 0,
-    net_deduction: 0,
-    gross_deduction: 0,
-    extra_bonus: 0,
-    bonus_qualifying_overtime: 0,
-    bonus_qualifying_amount: 0,
-    calculation_basis_salary: 0,
-    bonus_qualifying_time: 0,
-    bonus_qualifying_time_not_comp: 0,
+    reportedTime: 0,
+    travelTime: 0,
+    unassignedTime: 0,
+    vacationTime: 0,
+    sickTime: 0,
+    offDutyTime: 0,
+    compTime: 0,
+    valueOfVacation: 0,
+    netDeduction: 0,
+    grossDeduction: 0,
+    extraBonus: 0,
+    bonusQualifyingFee: 0,
+    bonusQualifyingAmount: 0,
+    calculationBasisSalary: 0,
+    bonusQualifyingTime: 0,
+    bonusQualifyingTimeNotComp: 0,
+    bonusQualifyingOverTime: 0,
   })
 
   useEffect(() => {
@@ -28,22 +29,17 @@ export function MonthlyBonusForm({ monthlyBonus, employee }) {
     }
   }, [monthlyBonus])
 
+  useEffect(() => {
+    updatedBonus(bonusData)
+  }, [bonusData])
+
   const handleChange = (event) => {
     setBonusData({ ...bonusData, [event.target.name]: event.target.value })
   }
 
-  function inputFactory(values) {
-    const [type, placeholder, value, name, disabled] = values
-    return (
-      <Input
-        onChange={handleChange}
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        name={name}
-        disabled={disabled}
-      />
-    )
+  function inputFactory(values, index) {
+    const [type, placeholder, value, name] = values
+    return <Input onChange={handleChange} type={type} placeholder={placeholder} value={value} name={name} key={index} />
   }
 
   return (
@@ -52,52 +48,53 @@ export function MonthlyBonusForm({ monthlyBonus, employee }) {
         <div className={styles.summary}>
           <h2>{employee}</h2>
           <p>
-            Bonus qualifying fee: <strong>{monthlyBonus.bonus_qualifying_fee}</strong>
+            Bonus qualifying fee: <strong>{monthlyBonus.bonusQualifyingFee}</strong>
           </p>
           <p>
-            Bonus outcome: <strong>{monthlyBonus.bonus_outcome}</strong>
+            Bonus outcome: <strong>{monthlyBonus.bonusOutcome}</strong>
           </p>
           <p>
-            Total balance: <strong>{monthlyBonus.bonus_balance}</strong>
+            Total balance: <strong>{monthlyBonus.bonusBalance}</strong>
           </p>
         </div>
         <div className={styles.container}>
           <div className={styles.form}>
             {[
-              ['number', 'Total time', bonusData.reported_time, 'reported_time', true],
-              ['number', 'Travel time', bonusData.travel_time, 'travel_time', true],
-              ['number', 'Unassigned time', bonusData.unassigned_time, 'unassigned_time', true],
-              ['number', 'Vacation time', bonusData.vacation_time, 'vacation_time', true],
-            ].map(inputFactory)}
-          </div>
-          <div className={styles.form}>
-            {[
-              ['number', 'Sick time', bonusData.reported_time, 'sick_time', true],
-              ['number', 'Offduty time', bonusData.offduty_time, 'offduty_time', true],
-              ['number', 'Comp time', bonusData.comp_time, 'comp_time', true],
-              ['number', 'Vacation value', bonusData.value_of_vacation, 'vacation_time', true],
+              ['number', 'Total time', bonusData.reportedTime, 'reportedTime', true],
+              ['number', 'Travel time', bonusData.travelTime, 'travelTime', true],
+              ['number', 'Unassigned time', bonusData.unassignedTime, 'unassignedTime', true],
+              ['number', 'Vacation time', bonusData.vacationTime, 'vacationTime', true],
             ].map(inputFactory)}
           </div>
 
           <div className={styles.form}>
             {[
-              ['number', 'Net deduction', bonusData.net_deduction, 'net_deduction', false],
-              ['number', 'Gross deduction', bonusData.gross_deduction, 'gross_deduction', true],
-              ['number', 'Extra bonus', bonusData.extra_bonus, 'extra_bonus', true],
-              ['number', 'Bonus overtime', bonusData.bonus_qualifying_overtime, 'bonus_qualifying_overtime', true],
+              ['number', 'Sick time', bonusData.sickTime, 'sickTime', true],
+              ['number', 'Offduty time', bonusData.offDutyTime, 'offDutyTime', true],
+              ['number', 'Comp time', bonusData.compTime, 'compTime', true],
+              ['number', 'Vacation value', bonusData.valueOfVacation, 'valueOfVacation', true],
             ].map(inputFactory)}
           </div>
 
           <div className={styles.form}>
             {[
-              ['number', 'Bonus amount', bonusData.bonus_qualifying_amount, 'bonus_qualifying_amount', true],
-              ['number', 'Basis salary', bonusData.calculation_basis_salary, 'calculation_basis_salary', true],
-              ['number', 'Bonus time', bonusData.bonus_qualifying_time, 'bonus_qualifying_time', true],
+              ['number', 'Net deduction', bonusData.netDeduction, 'netDeduction', false],
+              ['number', 'Gross deduction', bonusData.grossDeduction, 'grossDeduction', true],
+              ['number', 'Extra bonus', bonusData.extraBonus, 'extraBonus', true],
+              ['number', 'Bonus overtime', bonusData.bonusQualifyingOverTime, 'bonusQualifyingOverTime', true],
+            ].map(inputFactory)}
+          </div>
+
+          <div className={styles.form}>
+            {[
+              ['number', 'Bonus amount', bonusData.bonusQualifyingAmount, 'bonusQualifyingAmount', true],
+              ['number', 'Basis salary', bonusData.calculationBasisSalary, 'calculationBasisSalary', true],
+              ['number', 'Bonus time', bonusData.bonusQualifyingTime, 'bonusQualifyingTime', true],
               [
                 'number',
                 'Bonus time (not comp)',
-                bonusData.bonus_qualifying_time_not_comp,
-                'bonus_qualifying_time_not_comp',
+                bonusData.bonusQualifyingTimeNotComp,
+                'bonusQualifyingTimeNotComp',
                 true,
               ],
             ].map(inputFactory)}
